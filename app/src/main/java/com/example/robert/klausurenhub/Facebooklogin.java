c3997e23a1e7b76db702c1f41b3d918565aab9e4
Schierz, Robert <s65727@beuth-hochschule.de>
2017-07-28 14:48:40 +0200

    Facebook function for SessionID

--- app/src/main/java/com/example/robert/klausurenhub/Facebooklogin.java
+++ app/src/main/java/com/example/robert/klausurenhub/Facebooklogin.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +14,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -40,6 +44,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -72,6 +78,21 @@ public class Facebooklogin extends AppCompatActivity {
 
     @AfterViews
     public void afterViews() {
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.robert.klausurenhub",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
 
         FacebookSdk.sdkInitialize(getApplicationContext());
